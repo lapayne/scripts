bbeddfc3ae9f1a670faf63de7b880e1f4f1e6648
lapayne <56700654+lapayne@users.noreply.github.com>
2021-02-23 11:32:00 +0000

    change docker to use alpine

---
+++ Azure/general/get_intune_users.ps1
@@ -0,0 +1,3 @@
+Update-MSGraphEnvironment -AppId "727a0d3b-1e10-4f9d-b29b-c210309da082" -Quiet
+Update-MSGraphEnvironment -AuthUrl "https://login.windows.net/49f102d1-76bd-4da9-a1bc-f70b0999c18a" -Quiet
+Connect-MSGraph -ClientSecret "Mx.iNdH10bg_ONkEE8drka0l_y-yomxwXe" -Quiet
